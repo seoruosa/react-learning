bcb03a81782010353b093352d742872bd9308968
seoruosa <thiago.giachetto@gmail.com>
2018-07-14 22:52:51 -0300

    Starting the use of semantic ui

---
+++ semantic-test/src/HomepageTest.js
@@ -0,0 +1,70 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+import {
+  Button,
+  Container,
+  Divider,
+  Grid,
+  Header,
+  Icon,
+  Image,
+  List,
+  Menu,
+  Responsive,
+  Segment,
+  Sidebar,
+  Visibility,
+} from 'semantic-ui-react'
+
+const HomepageTest = () => (
+<div>
+  <Menu fixed='top'>
+    <Menu.Item>
+      ola1
+    </Menu.Item>
+    <Menu.Item>
+      ola2
+    </Menu.Item>
+    <Menu.Item>
+      ola3
+    </Menu.Item>
+  </Menu>
+  <Header as='h2' attached='top'>
+    Attached Header
+  </Header>
+  <Segment.Group vertical raised>
+    <Segment.Group>
+      <Segment>bla</Segment>
+      <Segment size='massive' inverted>ble</Segment>
+      <Segment>
+      <Grid columns={2}>
+        <Grid.Row>
+          <Grid.Column>
+            Mussum Ipsum, cacilds vidis litro abertis. Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio. A ordem dos tratores não altera o pão duris. Não sou faixa preta cumpadi, sou preto inteiris, inteiris. In elementis mé pra quem é amistosis quis leo.
+            Si num tem leite então bota uma pinga aí cumpadi! Casamentiss faiz malandris se pirulitá. Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis. Cevadis im ampola pa arma uma pindureta.
+            Leite de capivaris, leite de mula manquis sem cabeça. Quem manda na minha terra sou euzis! Sapien in monti palavris qui num significa nadis i pareci latim. Delegadis gente finis, bibendum egestas augue arcu ut est.
+            Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl. Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo! Detraxit consequat et quo num tendi nada. Copo furadis é disculpa de bebadis, arcu quam euismod magna.
+            Interagi no mé, cursus quis, vehicula ac nisi. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per. Aenean aliquam molestie leo, vitae iaculis nisl.
+            Mussum Ipsum, cacilds vidis litro abertis. Quem num gosta di mim que vai caçá sua turmis! Casamentiss faiz malandris se pirulitá. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis. Pra lá , depois divoltis porris, paradis.
+            Si u mundo tá muito paradis? Toma um mé que o mundo vai girarzis! Aenean aliquam molestie leo, vitae iaculis nisl. Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl. Diuretics paradis num copo é motivis de denguis.
+            Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio. Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo! Leite de capivaris, leite de mula manquis sem cabeça. Per aumento de cachacis, eu reclamis.
+            Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose. Atirei o pau no gatis, per gatis num morreus. Viva Forevis aptent taciti sociosqu ad litora torquent. Mé faiz elementum girarzis, nisi eros vermeio.
+            Delegadis gente finis, bibendum egestas augue arcu ut est. Quem manda na minha terra sou euzis! Não sou faixa preta cumpadi, sou preto inteiris, inteiris. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per.
+            Sapien in monti palavris qui num significa nadis i pareci latim.  Copo furadis é disculpa de bebadis, arcu quam euismod magna. Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis. Cevadis im ampola pa arma uma pindureta.
+            Quem num gosta di mé, boa gentis num é. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis. Si num tem leite então bota uma pinga aí cumpadi! Mais vale um bebadis conhecidiss, que um alcoolatra anonimis.
+            Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. Praesent malesuada urna nisi, quis volutpat erat hendrerit non. Nam vulputate dapibus. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Paisis, filhis, espiritis santis.
+            Detraxit consequat et quo num tendi nada. In elementis mé pra quem é amistosis quis leo. A ordem dos tratores não altera o pão duris. Manduma pindureta quium dia nois paga.
+            Interagi no mé, cursus quis, vehicula ac nisi. Suco de cevadiss deixa as pessoas mais interessantis. Vehicula non. Ut sed ex eros. Vivamus sit amet nibh non tellus tristique interdum. Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget.
+          </Grid.Column>
+          <Grid.Column>
+          Olar
+          </Grid.Column>
+        </Grid.Row>
+        </Grid>
+      </Segment>
+    </Segment.Group>
+  </Segment.Group>
+</div>
+)
+
+export default HomepageTest
--- semantic-test/src/index.js
+++ semantic-test/src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 //import './index.css';
 import 'semantic-ui-css/semantic.min.css';
 //import App from './App';
-import HomepageLayout from './HomepageLayout'
+//import HomepageLayout from './HomepageLayout'
 import registerServiceWorker from './registerServiceWorker';
+import HomepageTest from './HomepageTest'
 
-ReactDOM.render(<HomepageLayout />, document.getElementById('root'));
+//ReactDOM.render(<HomepageLayout />, document.getElementById('root'));
+ReactDOM.render(<HomepageTest />, document.getElementById('root'));
 registerServiceWorker();
